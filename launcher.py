ade507550ac197807538912de898d7dfdfc0cb62
Sander van de Graaf <sander.vandegraaf@runpod.io>
2026-09-16 15:49:22 +0200

    added comment for runpod import

--- launcher.py
+++ launcher.py
@@ -14,6 +14,7 @@ import os
 import sys
 import traceback
 
+# this will be installed by the Dockerfile
 import runpod
 
 APP_DIR = os.path.abspath(os.environ.get("RUNPOD_APP_DIR", "/runpod-volume/app"))
